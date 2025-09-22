074d642e8be79aa3353223a71cc688ecd87b2958
Tristan Salinas <salinas.tristan@gmail.com>
2025-09-22 16:41:58 +0200

    :construction: add empty logic file for architecture

--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,48 @@
 import Database from "better-sqlite3";
 
-const db = new Database(process.env.dbPath);
+const db = new Database(process.env.dbPath, {
+  verbose: console.log,
+});
 
 // Create messages table if it doesn't exist
 const initDatabase = () => {
   db.prepare(
     `
-  CREATE TABLE IF NOT EXISTS user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-  )
-`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password TEXT NOT NULL
+    );`
+  ).run();
+  db.prepare(
+    `
+    CREATE TABLE messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      author_id INTEGER NOT NULL,
+      conversation_id INTEGER NOT NULL,
+      content TEXT NOT NULL,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
+      FOREIGN KEY (conversation_id) REFERENCES conversations(id) ON DELETE CASCADE);
+    `
+  ).run();
+  db.prepare(
+    `
+    CREATE TABLE conversation_has_users (
+      conversation_id INTEGER NOT NULL,
+      user_id INTEGER NOT NULL,
+      PRIMARY KEY (conversation_id, user_id),
+      FOREIGN KEY (conversation_id) REFERENCES conversations(id) ON DELETE CASCADE,
+      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE);
+    `
+  ).run();
+  db.prepare(
+    `
+    CREATE TABLE conversations (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      is_group BOOLEAN NOT NULL DEFAULT FALSE,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );`
   ).run();
 };
 
