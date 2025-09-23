65ed803cedd0c58584e91f2fc9edda7afc42f6aa
Tristan Salinas <salinas.tristan@gmail.com>
2025-09-23 15:59:27 +0200

    :bug: hotfix cors

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import authRouter from "./controllers/auth.controller.js";
 import { checkToken } from "./middlewares/check-token.js";
+import cors from "cors";
 
 const app = express();
-const PORT = 3000;
 dotenv.config();
 
-// Middleware
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+    credentials: true,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Offline Chat Server is running!");
@@ -20,6 +25,6 @@ app.get("/protected-route", checkToken, (req: Request, res: Response) => {
   res.send("You accessed a protected route");
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server running on port ${process.env.PORT}`);
 });
