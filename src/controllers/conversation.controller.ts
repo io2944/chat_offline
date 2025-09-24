14a4163cb56ab1541d9ca404352a65b18bbeb43b
diana <diana.dumitrescu@outlook.com>
2025-09-24 10:52:13 +0200

    :sparkles: post conversation route added

--- src/controllers/conversation.controller.ts
+++ src/controllers/conversation.controller.ts
@@ -5,6 +5,10 @@ import {
 } from "../middlewares/check-token.js";
 import { getUncontactedUsersOf } from "../managers/users.manager.js";
 import { ensureCurrentUser } from "../utils/require-current-user.js";
+import {
+  createConversation,
+  findAllUserConversations,
+} from "../managers/conversations.manager.js";
 
 const conversationRouter = Router();
 
@@ -19,3 +23,44 @@ conversationRouter.get(
 );
 
 export default conversationRouter;
+
+conversationRouter.post(
+  "/conversations",
+  checkToken,
+  (req: Request, res: Response) => {
+    const { userId } = req.body;
+    const authReq = ensureCurrentUser(req);
+    const currentUserId = authReq.currentUser.id;
+
+    if (!userId || userId === currentUserId) {
+      return res.status(400).json({ error: "Invalid userId" });
+    }
+
+    const currentUserConversations = findAllUserConversations(
+      currentUserId
+    )?.filter((conv) => {
+      !conv.isGroup;
+    });
+    const targetUserConversations = findAllUserConversations(userId)?.filter(
+      (conv) => {
+        !conv.isGroup;
+      }
+    );
+
+    const currentIds = new Set(currentUserConversations?.map((c) => c.id));
+    const targetIds = new Set(targetUserConversations?.map((c) => c.id));
+
+    const sharedConversationId = [...currentIds].find((id) =>
+      targetIds.has(id)
+    );
+
+    if (sharedConversationId) {
+      return res.status(409).json({
+        error: "Conversation already exists",
+        conversationId: sharedConversationId,
+      });
+    }
+
+    return res.status(201).json(createConversation([currentUserId, userId]));
+  }
+);
